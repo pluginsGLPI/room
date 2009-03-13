981d315afb57e24aa7703f2cb1a2fefb0cd81b6a
Walid <walid.nouh@gmail.com>
2009-03-13 15:58:39 +0000

    [room, parametre] Add itlian translations (thanks to Marco Gaiarin

---
+++ locales/it_IT.php
@@ -0,0 +1,53 @@
+<?php
+/*
+
+   ----------------------------------------------------------------------
+   GLPI - Gestionnaire Libre de Parc Informatique
+   Copyright (C) 2003-2008 by the INDEPNET Development Team.
+
+   http://indepnet.net/   http://glpi-project.org/
+
+   ----------------------------------------------------------------------
+   LICENSE
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License (GPL)
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   To read the license please visit http://www.gnu.org/copyleft/gpl.html
+   ----------------------------------------------------------------------
+// Original Author of file: GRISARD Jean Marc & CAILLAUD Xavier
+Purpose of file:
+----------------------------------------------------------------------
+ */
+/**************** Plugin Addressing **************/
+
+
+$LANGROOM[0]="Locali";
+$LANGROOM[1]="Plugin installato e configurato: perfetto!";	
+$LANGROOM[2]="Plugin non installato!";	
+$LANGROOM[3]="Aggiungi un locale";
+$LANGROOM[4]="Posti a sedere";
+$LANGROOM[5]="Modalità di accesso";
+$LANGROOM[6]="Stampante";
+$LANGROOM[7]="Videoproiettore";
+$LANGROOM[8]="Servizio WiFi";
+$LANGROOM[9]="Tipologia di Locale";
+$LANGROOM[10]="Responsabile";
+$LANGROOM[11]="Orari di apertura";
+$LANGROOM[12]="Limitazioni";
+$LANGROOM[13]="Caratteristica 1";
+$LANGROOM[14]="Caratteristica 2";
+$LANGROOM[15]="Caratteristica 3";
+$LANGROOM[16]="Caratteristica 4";
+$LANGROOM[17]="Aggiungi un locale";
+$LANGROOM[18]="Numero PC";
+$LANGROOM[19]="Locale";
+$LANGROOM[20]="Computer presenti in sala:";
+?>
