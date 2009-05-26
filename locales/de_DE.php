557672c45965c7b420cff908b4d80613365493f3
Julien Dombre <moyo@glpi-project.org>
2009-05-26 07:20:16 +0000

    [room] add de_DE

---
+++ locales/de_DE.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * @version $Id: fr_FR.php 8382 2009-01-06 18:41:29Z moyo $
+ -------------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2009 by the INDEPNET Development Team.
+
+ http://indepnet.net/   http://glpi-project.org
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of GLPI.
+
+ GLPI is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ GLPI is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GLPI; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ --------------------------------------------------------------------------
+ */
+
+
+$LANG['plugin_room'][0]="Raumplan";
+$LANG['plugin_room'][1]="Plugin installiert und konfiguriert: Perfekt!";	
+$LANG['plugin_room'][2]="Plugin nicht installiert!";	
+$LANG['plugin_room'][3]="Raum hinzufügen";
+$LANG['plugin_room'][4]="Anzahl Plätze";
+$LANG['plugin_room'][5]="Zugangsbedingungen";
+$LANG['plugin_room'][6]="Drucker vorhanden";
+$LANG['plugin_room'][7]="Videoprojektor";
+$LANG['plugin_room'][8]="Wifi";
+$LANG['plugin_room'][9]="Art des Raumes";
+$LANG['plugin_room'][10]="Verantwortlicher";
+$LANG['plugin_room'][11]="Öffnungszeiten";
+$LANG['plugin_room'][12]="Beschränkungen";
+$LANG['plugin_room'][13]="Eigenschaft 1";
+$LANG['plugin_room'][14]="Eigenschaft 2";
+$LANG['plugin_room'][15]="Eigenschaft 3";
+$LANG['plugin_room'][16]="Eigenschaft 4";
+$LANG['plugin_room'][17]="Einen Raum hinzufügen";
+$LANG['plugin_room'][18]="Anzahl PC";
+$LANG['plugin_room'][19]="Raum";
+$LANG['plugin_room'][20]="Dieser PC befindet sich in Raum:";
+?>
